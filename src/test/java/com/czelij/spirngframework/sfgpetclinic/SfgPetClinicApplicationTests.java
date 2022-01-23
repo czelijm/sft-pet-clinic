5ba21fce3f2d947869ffb718c82aa08aa67a061d
czelijm <marekczelij1@gmail.com>
2022-01-23 18:54:22 +0100

    init commit

---
+++ src/test/java/com/czelij/spirngframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czelij.spirngframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
